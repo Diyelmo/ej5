314bdc7fcc17ecb04274b7e7424257bf848d6e7a
Diyelmo <141803284+Diyelmo@users.noreply.github.com>
2023-08-31 23:06:06 -0300

    Add files via upload

---
+++ agragarproducto/Productos.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package agragarproducto;
+
+/**
+ *
+ * @author RODRI
+ */
+public class Productos {
+   private String categoria;
+    private String nombre;
+     private int precio;
+
+    public Productos(String categoria,String nombre, int precio) {
+        this.categoria=categoria;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+     
+}
